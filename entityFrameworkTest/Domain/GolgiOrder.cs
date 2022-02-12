ffbe0797e192380474f2e5f880a2e2d70d63e16a
Yaiser <yaiserar@gmail.com>
2022-02-12 04:21:14 +0100

    feat golgiOrder and errorOrder

---
+++ entityFrameworkTest/Domain/GolgiOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entityFrameworkTest.Domain
+{
+    public class GolgiOrder
+    {
+        public string Order { get; set; }
+        public List<Component> Detail { get; set; }
+    }
+}
