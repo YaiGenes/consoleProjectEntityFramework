b781f04b0e1d288c7df4c8bbd1c6be7591b9e6c7
Yaiser <yaiserar@gmail.com>
2022-02-12 04:40:56 +0100

    feat domain synthesisResults

--- entityFrameworkTest/Domain/SynthesisResult.cs
+++ entityFrameworkTest/Domain/SynthesisResult.cs
@@ -8,6 +8,39 @@ namespace entityFrameworkTest.Domain
 {
     public class SynthesisResult
     {
+        public List<ErrorObject> Errors { get; set; } = new List<ErrorObject>();
+        public bool HasError()=> Errors.Count > 0;
+        public SynthesisResult() { }
+        public SynthesisResult(Exception exception)
+        {
+            AddError(exception);
+        }
+
+        public SynthesisResult(int code, string message, Exception exception = null)
+        {
+            AddError(code, message, exception);
+        }
+
+
+        public void AddError(int code, string message, Exception exception = null)
+        {
+            Errors.Add(new ErrorObject { Code = code, Message = message, Exception = exception})
+        }
+        public void AddError(Exception exception)
+        {
+             if(exception == null) throw new ArgumentNullException("exception");
+             Errors.Add(new ErrorObject { Exception = exception, Message = exception.Message });
+        }
+    }
+
+    public class SynthesisResult<T> : SynthesisResult
+    {
+        public T Result { get; set; }
+        public SynthesisResult() { }
+        public SynthesisResult(T result) => Result = result;
+        public SynthesisResult(Exception exception) : base(exception) { }
+        public SynthesisResult(int code, string message, Exception exception = null) : base(code, message, exception) { }
+        public void SetResult(T result) => Result = result;
 
     }
 }
