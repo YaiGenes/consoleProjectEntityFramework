4091b47994158979e52f92c00910aa89d9c6a2d2
Yaiser <yaiserar@gmail.com>
2022-02-13 17:51:27 +0100

    framework 5.0

---
+++ entityFrameworkTest/Mapping/AssemblingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using entityFrameworkTest.Data;
+using entityFrameworkTest.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entityFrameworkTest.Mapping
+{
+    public class AssemblingProfile : Profile
+    {
+        public AssemblingProfile()
+        {
+            CreateMap<Assembling, AssemblingEntity>()
+                  .ForMember(c => c.Id, opt => Guid.NewGuid());
+        }
+    }
+}
---
+++ entityFrameworkTest/Mapping/ComponentProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using entityFrameworkTest.Data;
+using entityFrameworkTest.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entityFrameworkTest.Mapping
+{
+    public class ComponentProfile : Profile
+    {
+        public ComponentProfile()
+        {
+            CreateMap<Component, ComponentEntity>();
+        }
+    }
+}
